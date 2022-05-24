745b52059b69e6663b19d23123540ca8be84d6c6
Christian Bound <automaticbound@gmail.com>
2022-05-24 14:41:18 +0900

    added some associations

--- db/migrate/20220523180535_add_column_to_users.rb
+++ db/migrate/20220523180535_add_column_to_users.rb
@@ -1,12 +1,11 @@
 class AddColumnToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :user, :name
-    add_column :user, :email
-    add_column :user, :phone
-    add_column :user, :address
-    add_column :user, :age, :integer
-    add_column :user, :marital_status
-    add_column :user, :children
-    add_column :user, :pets
+    add_column :users, :name, :string
+    add_column :users, :phone, :string
+    add_column :users, :address, :string
+    add_column :users, :age, :integer
+    add_column :users, :marital_status, :string
+    add_column :users, :children, :string
+    add_column :users, :pets, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_074257) do
+ActiveRecord::Schema.define(version: 2022_05_24_020806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "executors", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "address"
+    t.string "phone"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_executors_on_user_id"
+  end
+
+  create_table "funerals", force: :cascade do |t|
+    t.string "music"
+    t.string "no_invite"
+    t.text "sending_message"
+    t.string "arrangement"
+    t.string "representative"
+    t.string "ten_words"
+    t.text "memory"
+    t.text "unaware_state"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_funerals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +54,30 @@ ActiveRecord::Schema.define(version: 2022_05_23_074257) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "phone"
+    t.string "address"
+    t.integer "age"
+    t.string "marital_status"
+    t.string "children"
+    t.string "pets"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wills", force: :cascade do |t|
+    t.string "assets_range"
+    t.string "residuary"
+    t.string "primary_beneficiaries"
+    t.string "donation"
+    t.string "signature"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_wills_on_user_id"
+  end
+
+  add_foreign_key "executors", "users"
+  add_foreign_key "funerals", "users"
+  add_foreign_key "wills", "users"
 end
