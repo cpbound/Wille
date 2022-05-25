d4f8d3be2f3f715ca1d8a0a007a2101485f01460
Christian Bound <automaticbound@gmail.com>
2022-05-25 17:46:57 +0900

    will create and show functional

--- app/policies/will_policy.rb
+++ app/policies/will_policy.rb
@@ -2,11 +2,16 @@ class WillPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.where(user:user)
+      scope.where(user: user)
     end
   end
+
+  def show?
+    return true
+  end
+
   def create?
-  return true
+    return true
   end
 
   def edit?
