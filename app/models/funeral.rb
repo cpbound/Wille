e3e020502ff5b8e6f8d9747aa1e176aeb520b441
Christian Bound <automaticbound@gmail.com>
2022-06-01 16:42:57 +0900

    funeral text fixed. alerts added.

--- app/models/funeral.rb
+++ app/models/funeral.rb
@@ -6,14 +6,14 @@ class Funeral < ApplicationRecord
 
 
   QUESTIONS = {
-    memory: "What was your the best memory?",
-    arrangement: "In the event that you became too sick to speak for yourself, who would you like to speak for you?",
-    music: "The music you want to play at the funeral",
-    no_invite: "Is there anyone you’d never like to call to a funeral?",
-    representative: "Have you ever thought about Organ donation after circulatory death?",
-    sending_message: "Is there anything you wanted to say but couldn’t?",
-    ten_words: "10 words that describe your life so far",
-    unaware_state: "Imagine you become a vegetable patient"
+    memory: "What is your favourite memory?",
+    arrangement: "If you ever became too sick to speak for yourself, who would you like to speak for you?",
+    music: "What's the last song you'd ever want played for you",
+    no_invite: "Is there anyone you’d never call to your funeral?",
+    representative: "Would you be happy to be an organ donor after death?",
+    sending_message: "Is there anything you'd always wanted to say but couldn't?",
+    ten_words: "How would your describe yourself?",
+    unaware_state: "How would you want to be cared for if you were in a persistent unconcious state?"
   }
 
 
