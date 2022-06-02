89d6936c77d9eb86b0f0a8edde0556fe11dddd45
Christian Bound <automaticbound@gmail.com>
2022-06-03 02:07:26 +0900

    typo in mail fixed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,6 @@ class UserMailer < ApplicationMailer
     # This will break if the user doesn't exist.
     @user = User.find_by(email: params[:email]) || User.create(email: params[:email], password: "123123")
     @will = params[:will]
-    mail(to: @user.email, subject: "#{@will.user.name} wants you see their post life plan")
+    mail(to: @user.email, subject: "#{@will.user.name} wants you to see their post life plan!")
   end
 end
